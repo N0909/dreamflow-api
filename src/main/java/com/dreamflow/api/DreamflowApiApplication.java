2c13b90fd64f5dda8d476d39a6b58439dbbc53aa
N <n3089@gmail.com>
2026-04-06 14:06:08 +0530

    initial project setup

---
+++ src/main/java/com/dreamflow/api/DreamflowApiApplication.java
@@ -0,0 +1,13 @@
+package com.dreamflow.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamflowApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamflowApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dreamflow/api/DreamflowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dreamflow.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamflowApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

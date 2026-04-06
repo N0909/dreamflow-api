039ebf072f8dc7a4264f559b1362c131b946b0a5
N <n3089@gmail.com>
2026-04-06 22:39:10 +0530

    created Song.java

---
+++ src/main/java/com/dreamflow/api/songs/entity/Song.java
@@ -0,0 +1,26 @@
+package com.dreamflow.api.songs.entity;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="songs")
+@Data
+@NoArgsConstructor
+public class Song {
+    @Id
+    @Column(name="song_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int songId;
+    @Column(name="song_name")
+    private String songName;
+    @Column(name="song_path")
+    private String songPath;
+    @Column(name="duration_ms")
+    private long durationMs;
+    @CreationTimestamp
+    @Column(name="created_at")
+    private LocalDateTime createdAt;
+}

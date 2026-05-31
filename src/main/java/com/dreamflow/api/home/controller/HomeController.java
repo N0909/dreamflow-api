4ee8751e62f050490b38788f261f908323caa0da
N <n3089@gmail.com>
2026-05-31 14:55:29 +0530

    remove userid from gethomedata

--- src/main/java/com/dreamflow/api/home/controller/HomeController.java
+++ src/main/java/com/dreamflow/api/home/controller/HomeController.java
@@ -27,7 +27,7 @@ public class HomeController {
         return ResponseEntity
                 .ok()
                 .contentType(MediaType.APPLICATION_JSON)
-                .body(homeService.getHomeData(user.getUserId()));
+                .body(homeService.getHomeData());
     }
 
 }
